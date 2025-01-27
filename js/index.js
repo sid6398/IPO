82bd0ce2cc95a28b23c1107dae6b1952eb1c3730
Siddhesh Mithbavkar <siddheshmithbavkar98@gmail.com>
2025-01-27 12:10:32 +0530

    structural changes

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 const textArray = [
-    "Welcome to Our Website",
-    "Learn Frontend Development",
-    "Grow with ReactJS",
-    "Design Stunning Websites",
-  ];
-  let index = 0;
-  
-  const textElement = document.getElementById("changing-text");
-  
-  function changeText() {
-    textElement.textContent = textArray[index];
-    index = (index + 1) % textArray.length;
-  }
-  
-  setInterval(changeText, 3000); 
-  changeText(); 
-  
+  "Zero Account Opening Charges",
+  "Hassle-Free Setup",
+  "Real-Time IPO access",
+];
+
+let index = 0;
+
+const textElement = document.getElementById("changing-text");
+
+function changeText() {
+  textElement.textContent = textArray[index];
+  index = (index + 1) % textArray.length;
+}
+
+setInterval(changeText, 3000);
+changeText();
